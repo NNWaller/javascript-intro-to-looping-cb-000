c5594d41f06a618cc3d1e973097683ae880993ce
Natasha Waller <nnwaller@gmail.com>
2018-12-06 19:11:21 +0000

    Automatically backed up by Learn

---
+++ loops.js
@@ -0,0 +1,27 @@
+
+  function forLoop(){
+    for (let i = 0; i < 25; i++){
+      console.log(`I am ${i} strange loop ${i ===1 ? '' : 's'}.`);
+    }
+  }
+
+
+  var number = 10;
+  function whileLoop(number){
+    while (number > 0) {
+      console.log(number);
+      number--;
+    }
+    return 'done';
+  }
+
+
+
+  var i = 0;
+  function doWhileLoop(array){
+    do{
+      function incrementVariable() {
+        i = i + 1;
+      } while(array.length > 0 && incrementVariable());
+      array.splice(i);
+    }
